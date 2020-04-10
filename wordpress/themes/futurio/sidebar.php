be74a7542e62290ed7f80b2acdc482f1615781d4
Michał <mic.m4lik@gmail.com>
2020-04-10 16:12:17 +0200

    Brief completed, content to do

--- wordpress/themes/futurio/sidebar.php
+++ wordpress/themes/futurio/sidebar.php
@@ -1,5 +1,5 @@
 <?php if ( is_active_sidebar( 'futurio-sidebar' ) || ( is_archive() || is_search() ) && is_active_sidebar( 'futurio-archive-sidebar' ) ) { ?>
-	<aside id="sidebar" class="col-md-<?php echo absint( get_theme_mod( 'sidebar_size', '3' ) ) ?> <?php futurio_sidebar_position( 'sidebar' ) ?>">
+	<aside id="sidebar" class="<?php if(null!==get_field('color')) echo get_field('color'); else echo "green"; ?> col-md-<?php echo absint( get_theme_mod( 'sidebar_size', '3' ) ) ?> <?php futurio_sidebar_position( 'sidebar' ) ?>">
 		<?php
 		if ( ( is_archive() || is_search() ) && is_active_sidebar( 'futurio-archive-sidebar' ) ) {
 			dynamic_sidebar( 'futurio-archive-sidebar' );
